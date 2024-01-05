7cf0caa8b98389e827f3ba25a9ac1c04ff89eb53
Prayag887 <prayagdhakal123@gmail.com>
2024-01-05 17:06:24 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, jsonify
+import sqlite3
+import pandas as pd
+import json
+
+app = Flask(__name__)
+
+@app.route('/get_predictions', methods=['POST'])
+def get_predictions():
+    try:
+        # Get the entity name from the POST request
+        entity_name = json.loads(request.data.decode("UTF-8"))["entity_name"]
+
+        print(entity_name)
+
+        # Connect to the SQLite database
+        conn = sqlite3.connect('renewable_predictions_test.db')
+        
+
+        # Query the database for predictions of the provided entity
+        query = f"SELECT * FROM predictions WHERE Entity = ?"
+        result = pd.read_sql_query(query, conn, params=(entity_name,))
+
+        
+        # Close the database connection
+        conn.close()
+        
+ 
+        # Convert the result to JSON and return it
+        if not result.empty:
+            return jsonify(result.to_dict(orient='records'), result.to_dict(orient='records'))
+        else:
+            return jsonify({'message': 'Entity not found'}), 404
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+
+@app.route('/get_confidence_level', methods=['POST'])
+def get_confidence_level():
+    try:
+        # Get the entity name from the POST request
+        entity_name = json.loads(request.data.decode("UTF-8"))["entity_name"]
+
+        print( entity_name)
+
+        # Connect to the SQLite database
+        conn = sqlite3.connect('confidence_levl.db')
+
+        # Query the database for data of the provided entity
+        query = "SELECT * FROM Predictions WHERE Entity = ?"
+        result = pd.read_sql_query(query, conn, params=(entity_name,))
+
+        # Close the database connection
+        conn.close()
+
+        # Convert the result to JSON and return it
+        if not result.empty:
+            return jsonify(result.to_dict(orient='records'))
+        else:
+            return jsonify({'message': 'Entity not found'}), 404
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/get_mase_error', methods=['POST'])
+def get_mase_error():
+    try:
+        # Get the entity name from the POST request
+        entity_name = json.loads(request.data.decode("UTF-8"))["entity_name"]
+
+        print( entity_name)
+
+        # Connect to the SQLite database
+        conn = sqlite3.connect('mase__error.db')
+
+        # Query the database for data of the provided entity
+        query = "SELECT * FROM MASE_ERROR WHERE Entity = ?"
+        result = pd.read_sql_query(query, conn, params=(entity_name,))
+
+        # Close the database connection
+        conn.close()
+
+        # Convert the result to JSON and return it
+        if not result.empty:
+            return jsonify(result.to_dict(orient='records'))
+        else:
+            return jsonify({'message': 'Entity not found'}), 404
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
